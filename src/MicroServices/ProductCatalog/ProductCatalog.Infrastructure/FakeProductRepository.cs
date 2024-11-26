267e3b54764d4f3d856676dd7acaa5df485ce933
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-26 12:50:22 +0100

    Dodanie infrastruktury

---
+++ src/MicroServices/ProductCatalog/ProductCatalog.Infrastructure/FakeProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductCatalog.Domain.Abstractions;
+using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Infrastructure;
+
+// C# 12 Primary Constructor
+public class FakeProductRepository(Context context) : IProductRepository
+{
+    public Task<IEnumerable<Product>> GetAll() => Task.FromResult<IEnumerable<Product>>(context.Products.Values);
+    public Task<Product> GetById(int id) => Task.FromResult(context.Products[id]);
+    public Task<IEnumerable<Product>> GetByPrice(decimal from, decimal to)
+    {
+        throw new NotImplementedException();
+    }
+}
