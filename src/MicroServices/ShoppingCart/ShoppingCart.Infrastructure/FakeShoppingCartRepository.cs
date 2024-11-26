8747607c9c9982e12bcd2a33efc583e5f7b8ed09
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-26 14:17:37 +0100

    Dodanie infratruktury koszyka

---
+++ src/MicroServices/ShoppingCart/ShoppingCart.Infrastructure/FakeShoppingCartRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingCart.Domain.Abstractions;
+using ShoppingCart.Domain.Models;
+
+namespace ShoppingCart.Infrastructure;
+
+public class FakeShoppingCartRepository : IShoppingCartRepository
+{
+    private Cart Cart = new Cart(); 
+
+    public Task Add(CartItem item)
+    {
+        Cart.Items.Add(item);
+
+        return Task.CompletedTask;
+    }
+}
+
+
