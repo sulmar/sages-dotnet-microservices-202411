d538696afdcb6c130ce7040f8531871caf07d933
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-26 12:50:34 +0100

    Dodanie Api katalogu produktów

---
+++ src/MicroServices/ProductCatalog/ProductCatalog.Api/Program.cs
@@ -0,0 +1,50 @@
+using ProductCatalog.Domain.Abstractions;
+using ProductCatalog.Domain.Entities;
+using ProductCatalog.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IProductRepository, FakeProductRepository>();
+builder.Services.AddTransient<Context>(sp =>
+{
+    var products = new List<Product>
+    {
+        new Product(1, "Popular Product", 80.00m),
+        new Product(2, "Special Item", 80.00m, 50m),
+        new Product(3, "Extra Product", 80.00m),
+        new Product(4, "Bonus Product", 80.00m, 70m),
+        new Product(5, "Fancy Product", 80.00m, 70m),
+        new Product(6, "Smart Product", 99.99m),
+        new Product(7, "Old-school Product", 199.00m),
+        new Product(8, "Future Product", 1.00m)
+    };
+
+    return new Context { Products = products.ToDictionary(p => p.Id) };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello ProductCatalog.Api");
+
+// TODO: Dodaj pobieranie produktów z repozytorium
+// GET api/products
+app.MapGet("api/products", async (IProductRepository repository) => await repository.GetAll());
+
+
+
+app.Run();
+
