bfa21d6f4be37f8bac2bec4e69c560d94386e97c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-26 14:17:25 +0100

    Dodanie domeny koszyka

---
+++ src/MicroServices/ShoppingCart/ShoppingCart.Domain/Abstractions/IShoppingCartRepository.cs
@@ -0,0 +1,8 @@
+﻿using ShoppingCart.Domain.Models;
+
+namespace ShoppingCart.Domain.Abstractions;
+
+public interface IShoppingCartRepository
+{
+    Task Add(CartItem item);
+}
---
+++ src/MicroServices/ShoppingCart/ShoppingCart.Domain/Models/ProductItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingCart.Domain.Models;
+
+public record CartItem(int Id, string Name, decimal Price, int Quantity = 1);
+
+public class Cart
+{
+    public ICollection<CartItem> Items { get; set; } = [];
+}
